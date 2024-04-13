da9d144e972677bf2a2c5dd010bebd02d6cc5640
Dominik Peters <mail@dominik-peters.de>
2024-04-13 23:38:49 +0200

    delete demo on load

--- js/loadPython.js
+++ js/loadPython.js
@@ -31,48 +31,15 @@ export async function loadPython() {
     loading.innerHTML = "Loading... (80%)";
     await window.pyodide.runPythonAsync(`
         from pyodide.http import pyfetch
-        response = await pyfetch("/pref_voting/pref_voting.zip")
-        await response.unpack_archive()
+        response = await pyfetch("/pref_voting/pref_voting.zip?v=15")
+        await response.unpack_archive(format="zip")
     `);
     await window.pyodide.runPython(`
         from pref_voting.profiles import Profile
-        prof = Profile([[0, 1, 2], [2, 1, 0]])
-        print(f"The rankings are {prof.rankings}")
-        print(f"The number of voters is {prof.num_voters}")
-        print(f"The number of candidate is {prof.num_cands}")
-        print(f"The candidates are {prof.candidates}")
-        prof.display()
         from pref_voting.scoring_methods import plurality, borda, anti_plurality
         from pref_voting.iterative_methods import *
         from pref_voting.c1_methods import *
         from pref_voting.margin_based_methods import *
-        from pref_voting.io.writers import *
-
-        cmap = {0:"a", 1:"b", 2:"c", 3:"d"}
-
-        prof = Profile([[0, 1, 2, 3], [2, 0, 1, 3], [1, 3, 0, 2], [3, 1, 0, 2]], [3, 2, 3, 1])
-
-        prof.display()
-
-        print(plurality.name)
-        print(plurality(prof))
-        print(plurality(prof, curr_cands = [2, 3]))
-        print(plurality.choose(prof))
-        plurality.display(prof)
-        plurality.display(prof, cmap = cmap)
-        instant_runoff.display(prof)
-        coombs.display(prof)
-        instant_runoff_put.display(prof)
-        copeland.display(prof)
-        uc_gill.display(prof)
-        top_cycle.display(prof)
-        banks.display(prof)
-        minimax.display(prof)
-        beat_path.display(prof)
-        ranked_pairs.display(prof)
-        split_cycle.display(prof)
-        river.display(prof)
-        stable_voting.display(prof)
     `);
     // enable all buttons and inputs
     document.querySelectorAll("button, input").forEach(function (el) {
